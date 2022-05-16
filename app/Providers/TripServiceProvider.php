c0b9ac587d10833b73fb35f5b11574473ba365d3
milena.schmidt <milena.schmidt@tsh.io>
2022-05-16 16:45:31 +0200

    Trip service provider

---
+++ app/Providers/TripServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TripServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+}
