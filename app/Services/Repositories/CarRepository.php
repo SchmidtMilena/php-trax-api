deb55d80ce0c1afce764a5f6d23e40f2e592bfc4
milena.schmidt <milena.schmidt@tsh.io>
2022-05-17 11:45:13 +0200

    mapping for domain model in repositories and basic functionalities

--- app/Services/Repositories/CarRepository.php
+++ app/Services/Repositories/CarRepository.php
@@ -12,22 +12,31 @@ final class CarRepository implements CarRepositoryContract
 {
     public function findForUser(int $userId): Collection
     {
-
+        return CarModel::where('user_id', $userId)->get()->map(
+            function (CarModel $carModel): Car {
+                return $this->mapToDomainModel($carModel);
+            }
+        );
     }
 
     public function store(array $data): void
     {
-
+        CarModel::create($data);
     }
 
     public function delete(int $id): void
     {
-
+        CarModel::destroy($id);
     }
 
     private function mapToDomainModel(CarModel $car): Car
     {
-
+        return new Car(
+            $car->id,
+            $car->user_id,
+            $car->year,
+            $car->make,
+            $car->model
+        );
     }
-
 }
--- app/Services/Repositories/TripRepository.php
+++ app/Services/Repositories/TripRepository.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Services\Repositories;
 
+use App\Services\DomainModels\Car;
 use App\Services\DomainModels\Trip;
 use App\Services\Repositories\Contracts\TripRepositoryContract;
+use Carbon\CarbonImmutable;
 use Illuminate\Support\Collection;
 use App\Models\Trip as TripModel;
 
@@ -13,16 +15,37 @@ final class TripRepository implements TripRepositoryContract
 {
     public function findForUser(int $userId): Collection
     {
-
+        return TripModel::where('user_id', $userId)->get()->map(
+            function (TripModel $tripModel): Trip {
+                return $this->mapToDomainModel($tripModel);
+            }
+        );
     }
 
     public function store(array $data): void
     {
-
+        TripModel::create($data);
     }
 
     private function mapToDomainModel(TripModel $trip): Trip
     {
+        /** @var \App\Models\Car $car */
+        $car = $trip->car();
 
+        $car = new Car(
+            $car->id,
+            $car->user_id,
+            $car->year,
+            $car->make,
+            $car->model
+        );
+
+        return new Trip(
+            $trip->id,
+            $trip->user_id,
+            $car,
+            new CarbonImmutable($trip->date),
+            $trip->miles
+        );
     }
 }
